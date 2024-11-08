6af1b5f613620198c3c96e60ba87a4d60d15adc5
LUCASBLOP <lucasctbatista@hotmail.com>
2024-11-08 20:23:31 -0300

    Terceira alteração

--- Testegit.c
+++ Testegit.c
@@ -1,9 +1,5 @@
 void main(void){
 
     printf("Hello World");
-<<<<<<< HEAD
-    printf("Hello World");
-
-=======
->>>>>>> b568a6536131baf61146af68f5ad73b572b158f9
+    printf("Porra de git hub!!!!!!!!!");
 }
